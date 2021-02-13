b29bd98cb85322a65e6d2e255d3b7881c62a37c5
Pavel Sirenko <54384981+enfild@users.noreply.github.com>
2021-02-14 00:54:16 +0700

    Update serialParser.cpp

--- serialParser.cpp
+++ serialParser.cpp
@@ -13,11 +13,6 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    // Qtport.setBaudRate(QSerialPort::Baud38400);
-    // Qtport.setDataBits(QSerialPort::Data8);
-    // Qtport.setStopBits(QSerialPort::OneStop);
-    // Qtport.setParity(QSerialPort::NoParity);
-    // Qtport.setFlowControl(QSerialPort::NoFlowControl);
     Qtport.setBaudRate(QSerialPort::Baud38400);
     Qtport.setDataBits(QSerialPort::Data8);
     Qtport.setStopBits(QSerialPort::OneStop);
@@ -42,11 +37,7 @@ void MainWindow::testSerialPort()
     QTreeWidgetItem *Item = new QTreeWidgetItem(ui->treeWidget);
     qDebug() << "before read all";
     data.append(Qtport.readAll());
-    // data.append("kkigASDPP");
-    //    uchar nas[9] = {1, 2, 253, 17, 99, 100, 236, 2, 2};
-    //    data.append((char*)nas, 9);
     qDebug() << "after read all";
-    // data = Qtport.readAll();
     qDebug() << data.size() << "razmer";
     if(data.size() < SIZE_OF_PACK)
     {
@@ -64,25 +55,16 @@ void MainWindow::testSerialPort()
             data.remove(0, 1);
         }
         qDebug() << "if true(composition of arr)";
-
         ROT = uint16_t(mas[NUMBER_FByte_ROT] << 8) + mas[NUMBER_SByte_ROT];
-
         XrowACCL = uint16_t(mas[NUMBER_FByte_XACCL] << 8) + mas[NUMBER_SByte_XACCL];
-
         YrowACCL = uint16_t(mas[NUMBER_FByte_YACCL] << 8) + mas[NUMBER_SByte_YACCL];
-
-//        qDebug() << ROT;
         ROTS = inclineScale(ROT);
         XACCL = accelScale(XrowACCL);
         YACCL = accelScale(YrowACCL);
-
         qDebug() << ROTS << "from func";
-//        qDebug() << fres << "from Sanya";
-
         ROTS = filtred_value(ROTS, 0);
         XACCL = filtred_value(XACCL, 1);
         YACCL = filtred_value(YACCL, 2);
-
         ui->tableWidget->setRowCount(ui->tableWidget->rowCount() + 1);
         for (int i =0; i<SIZE_OF_PACK; i++){
             for (int j = 0; j < NUMBER_VALUE; j++){
@@ -96,7 +78,6 @@ void MainWindow::testSerialPort()
                     Item->setText(j, QString::number(YACCL));
                 }
             }
-            //            QTableWidgetItem asd("asd"); string for testg
             if(ui->tableWidget->item(ui->tableWidget->rowCount() - 1, i) == nullptr)
             {
                 ui->tableWidget->setItem(ui->tableWidget->rowCount() - 1, i , new QTableWidgetItem(QString::number(mas[i])));
@@ -113,7 +94,6 @@ void MainWindow::testSerialPort()
 void MainWindow::on_pushButton_clicked()
 {
     Qtport.setPortName(ui->comboBox->currentText());
-    //Qtport.close();
     qDebug() << "open port";
     qDebug() << "open port 2";
     bool ok = Qtport.open(QIODevice::ReadWrite);
@@ -131,16 +111,6 @@ void MainWindow::on_comboBox_currentIndexChanged(const QString &arg1)
     ui->textEdit_infoComPort->append(info.description());
 }
 
-//float MainWindow::filtred_value(float ROW, int number){
-//    Pc[number] = P[number] + varProcess[number];
-//    G[number] = Pc[number]/(Pc[number] + varVolt[number]);
-//    P[number] = (1-G[number])*Pc[number];
-//    Xp[number] = Xe[number];
-//    Zp[number] = Xp[number];
-//    Xe[number] = G[number]*(ROW-Zp[number])+Xp[number]; // "фильтрованное" значение
-//    return(Xe[number]);
-//}
-
 float MainWindow::inclineScale(int16_t sensorData)
 {
     int signedData = 0;
@@ -153,7 +123,6 @@ float MainWindow::inclineScale(int16_t sensorData)
     float finalData = signedData * 0.025;
     return finalData;
 }
-// X = 0; Y = 1
 float MainWindow::accelScale(int16_t sensorData)
 {
     int signedData = 0;
